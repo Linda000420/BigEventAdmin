59d67771f89fd271b7d228394c28d8581a8390e0
Lin <1030863031@qq.com>
2024-05-09 18:02:18 +0800

    完成登录拦截守卫功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '@/stores'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,6 +43,16 @@ const router = createRouter({
       ]
     }
   ]
+})
+
+// 登录访问拦截 => 默认直接放行
+// 1. undefined / true：直接放行
+// 2. false：返回 from 页面
+// 3. 具体路径 / 路径对象：拦截到对应地址
+router.beforeEach((to) => {
+  // 没有 token 且访问的是非登录页，拦截到登录
+  const useStore = useUserStore()
+  if (!useStore.token && to.path !== '/login') return '/login'
 })
 
 export default router
