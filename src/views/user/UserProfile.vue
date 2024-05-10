7527433f0a8c73f065397903540a895e887bf86e
Lin <1030863031@qq.com>
2024-05-11 00:55:03 +0800

    完成用户界面静态布局

--- src/views/user/UserProfile.vue
+++ src/views/user/UserProfile.vue
@@ -1,5 +1,62 @@
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+import { ref } from 'vue'
+import { useUserStore } from '@/stores'
+
+const formRef = ref()
+const {
+  user: { email, id, nickname, username }
+} = useUserStore()
+
+const form = ref({
+  username, // 用户名
+  nickname, // 昵称
+  email, // 邮箱
+  id
+})
+
+const rules = {
+  nickname: [
+    { required: true, message: '请输入用户昵称', trigger: 'blur' },
+    {
+      pattern: /^\S{2,10}/,
+      message: '用户昵称长度在 2-10 个非空字符',
+      trigger: 'blur'
+    }
+  ],
+  email: [
+    { required: true, message: '请输入用户邮箱', trigger: 'blur' },
+    {
+      type: 'email',
+      message: '请输入正确的邮箱格式',
+      trigger: ['blur', 'change']
+    }
+  ]
+}
+
+const submitForm = () => {
+  console.log(123)
+}
+</script>
+
 <template>
-  <div>个人详情</div>
+  <page-container title="基本资料">
+    <!-- 表单 -->
+    <el-form :model="form" :rules="rules" ref="formRef" label-width="100px">
+      <el-form-item label="登录名称">
+        <el-input v-model="form.username" disabled></el-input>
+      </el-form-item>
+      <el-form-item label="用户昵称" prop="nickname">
+        <el-input v-model="form.nickname"></el-input>
+      </el-form-item>
+      <el-form-item label="用户邮箱" prop="email">
+        <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm">提交修改</el-button>
+      </el-form-item>
+    </el-form>
+  </page-container>
 </template>
 
 <style scoped></style>
